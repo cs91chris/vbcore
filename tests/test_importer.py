aee53da878302cf7867f5efae7adacdd672281b2
cs91chris <cs91chris@voidbrain.me>
2022-02-22 02:28:20 +0100

    improved importer and added ImporterFactory

--- tests/test_importer.py
+++ tests/test_importer.py
@@ -1,8 +1,89 @@
+import pytest
+
 from vbcore import misc
-from vbcore.importer import Importer
+from vbcore.importer import (
+    Importer,
+    ImporterFactory,
+    ImporterAttributeError,
+    ImporterModuleError,
+    ImporterValueError,
+    ImporterInstanceError,
+    ImporterSubclassError,
+)
 from vbcore.tester.mixins import Asserter
+
+
+class Base:
+    @classmethod
+    def perform(cls):
+        return "base"
+
+
+class AClass(Base):
+    @classmethod
+    def perform(cls):
+        return "A"
+
+
+class BClass(Base):
+    @classmethod
+    def perform(cls):
+        return "B"
 
 
 def test_import_module():
     to_int_func = Importer.from_module("vbcore.misc:to_int")
     Asserter.assert_equals(to_int_func, misc.to_int)
+
+
+def test_import_module_error():
+    with pytest.raises(ImporterModuleError) as error:
+        Importer.from_module("module.not.found")
+
+    Asserter.assert_equals(error.value.name, "module.not.found")
+    Asserter.assert_true(isinstance(error.value.exception, ModuleNotFoundError))
+
+
+def test_import_attribute_error():
+    with pytest.raises(ImporterAttributeError) as error:
+        Importer.from_module("vbcore.misc:not_found")
+
+    Asserter.assert_equals(error.value.module.__name__, "vbcore.misc")
+    Asserter.assert_equals(error.value.attribute, "not_found")
+
+
+def test_import_instance_error():
+    with pytest.raises(ImporterInstanceError) as error:
+        Importer.from_module("vbcore.misc:to_int", instance_of=str)
+
+    Asserter.assert_equals(error.value.item, misc.to_int)
+    Asserter.assert_equals(error.value.instance_of, str)
+
+
+def test_import_subclass_error():
+    with pytest.raises(ImporterSubclassError) as error:
+        Importer.from_module("tests.test_importer:AClass", subclass_of=dict)
+
+    Asserter.assert_equals(error.value.item, AClass)
+    Asserter.assert_equals(error.value.subclass_of, dict)
+
+
+def test_import_factory():
+    factory = ImporterFactory(
+        base_class=Base,
+        package="tests",
+        classes={"A": "test_importer:AClass", "B": "test_importer:BClass"},
+    )
+
+    Asserter.assert_equals(factory.get_class("A"), AClass)
+    Asserter.assert_equals(factory.get_class("A").perform(), "A")
+    Asserter.assert_true(isinstance(factory.get_instance("B"), BClass))
+
+
+def test_import_factory_error():
+    factory = ImporterFactory()
+
+    with pytest.raises(ImporterValueError) as error:
+        factory.get_class("A")
+
+    Asserter.assert_equals(error.value.name, "A")
--- vbcore/importer.py
+++ vbcore/importer.py
@@ -1,48 +1,141 @@
 import importlib
 import typing as t
+from types import ModuleType
+
+
+class ImporterError(Exception):
+    pass
+
+
+class ImporterModuleError(ImporterError):
+    def __init__(self, name, exc: Exception):
+        self.exception = exc
+        self.name = name
+        super().__init__(exc)
+
+
+class ImporterValueError(ImporterError):
+    def __init__(self, name: str):
+        self.name = name
+        super().__init__(f"class not registered: '{name}'")
+
+
+class ImporterSubclassError(ImporterError):
+    def __init__(self, item: t.Any, subclass_of: t.Type):
+        self.item = item
+        self.subclass_of = subclass_of
+        class_name = f"{subclass_of.__module__}:{subclass_of.__name__}"
+        super().__init__(f"'{item}' must be subclass of '{class_name}'")
+
+
+class ImporterInstanceError(ImporterError):
+    def __init__(self, item: t.Any, instance_of: t.Type):
+        self.item = item
+        self.instance_of = instance_of
+        class_name = f"{instance_of.__module__}:{instance_of.__name__}"
+        super().__init__(f"'{item}' must be instance of '{class_name}'")
+
+
+class ImporterAttributeError(ImporterError):
+    def __init__(self, module: ModuleType, attribute: t.Optional[str] = None):
+        self.module = module
+        self.attribute = attribute
+        super().__init__(f"attribute '{attribute}' not found in module '{module}'")
 
 
 class Importer:
-    attribute_separator = ":"
+    attribute_separator: str = ":"
+
+    @classmethod
+    def check_subclass(cls, item: t.Any, subclass_of: t.Optional[t.Type]):
+        if subclass_of and isinstance(item, type) and not issubclass(item, subclass_of):
+            raise ImporterSubclassError(item, subclass_of)
+
+    @classmethod
+    def check_isinstance(cls, item: t.Any, instance_of: t.Optional[t.Type]):
+        if instance_of and not isinstance(item, instance_of):
+            raise ImporterInstanceError(item, instance_of)
+
+    @classmethod
+    def parse_string(cls, name: str) -> t.Tuple[str, t.Optional[str]]:
+        if cls.attribute_separator in name:
+            mod, attr = name.split(cls.attribute_separator)
+            return mod, attr
+        return name, None
 
     @classmethod
     def from_module(
         cls,
         name: str,
         *args,
-        subclass_of: t.Type = None,
-        create: bool = False,
+        subclass_of: t.Optional[t.Type] = None,
+        instance_of: t.Optional[t.Type] = None,
+        package: t.Optional[str] = None,
+        call_with: bool = False,
         raise_exc: bool = True,
         **kwargs,
     ) -> t.Any:
-        """
+        mod, attr = cls.parse_string(name)
 
-        :param name:
-        :param args:
-        :param subclass_of:
-        :param create:
-        :param raise_exc:
-        :param kwargs:
-        :return:
-        """
-        if cls.attribute_separator in name:
-            mod, attr = name.split(cls.attribute_separator)
-        else:
-            mod, attr = name, None
+        try:
+            module = importlib.import_module(mod, package)
+        except ModuleNotFoundError as exc:
+            if raise_exc:
+                raise ImporterModuleError(mod, exc) from exc
+            return None
 
-        module = importlib.import_module(mod)
         if attr is None:
             return module
 
         if raise_exc:
-            ia = getattr(module, attr)
+            try:
+                imported = getattr(module, attr)
+            except AttributeError as exc:
+                raise ImporterAttributeError(module, attr) from exc
         else:
-            ia = getattr(module, attr, None)
+            imported = getattr(module, attr, None)
 
-        if raise_exc and subclass_of and not issubclass(ia, subclass_of):
-            raise TypeError(f"'{name}' must be subclass of {subclass_of}")
+        cls.check_subclass(imported, subclass_of)
+        cls.check_isinstance(imported, instance_of)
 
-        if args or kwargs:
-            return ia(*args, **kwargs)
+        if call_with is False and not (args and kwargs):
+            return imported
 
-        return ia() if create is True else ia
+        return imported(*args, **kwargs) if args and kwargs else imported()
+
+
+class ImporterFactory:
+    def __init__(
+        self,
+        base_class: t.Optional[t.Type] = None,
+        package: t.Optional[str] = None,
+        classes: t.Optional[t.Dict[str, str]] = None,
+    ):
+        self.base_class = base_class
+        self.package = package
+        self.classes: t.Optional[t.Dict[str, str]] = {}
+        if classes:
+            self.register_classes(classes)
+
+    def register_classes(self, classes: t.Optional[t.Dict[str, str]]):
+        for key, value in classes.items():
+            self.register_class(key, value)
+
+    def register_class(self, name: str, module_class: str):
+        self.classes[name] = f".{module_class}" if self.package else module_class
+
+    def get_instance(self, name: str, *args, **kwargs):
+        return self.get_class(name, *args, call_with=True, **kwargs)
+
+    def get_class(self, name: str, *args, call_with: bool = False, **kwargs):
+        if name not in self.classes:
+            raise ImporterValueError(name)
+
+        return Importer.from_module(
+            self.classes[name],
+            *args,
+            call_with=call_with,
+            package=self.package,
+            subclass_of=self.base_class,
+            **kwargs,
+        )
