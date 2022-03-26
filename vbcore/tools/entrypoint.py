4af0681e4325b929e4e0aa6f9eccb60813b9f7bc
cs91chris <cs91chris@voidbrain.me>
2022-03-26 03:38:38 +0100

    added dump_version to entrypoint

--- vbcore/tools/entrypoint.py
+++ vbcore/tools/entrypoint.py
@@ -1,9 +1,18 @@
+import sys
+
 import click
 
 from vbcore.tools.crypto import main as main_crypto
 from vbcore.tools.database import main as main_database
+from vbcore.version import __version__
 
-main = click.Group()
+
+def dump_version():
+    print(f"vbcore version: {__version__}")
+    print(f"python version: {sys.version}")
+
+
+main = click.Group(invoke_without_command=True, callback=dump_version)
 main.add_command(main_database)
 main.add_command(main_crypto)
 
