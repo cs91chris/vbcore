9e83c919ef6bd0fc5f71e4b979dbe210f6f68ab7
cs91chris <cs91chris@voidbrain.me>
2022-04-04 22:49:52 +0200

    added upload to pypi

--- devops/scripts/dist-publish.sh
+++ devops/scripts/dist-publish.sh
@@ -4,5 +4,13 @@ if [[ -n "${REPO_URL}" ]]; then
 	REPO_OPTS="--repository-url ${REPO_URL}"
 fi
 
+TWINE_OPTS="--verbose --skip-existing --non-interactive"
+
+# upload to gitlab
 # shellcheck disable=SC2086
-twine upload --verbose --skip-existing --non-interactive ${REPO_OPTS} dist/*
+twine upload ${TWINE_OPTS} ${REPO_OPTS} dist/*
+
+# upload to pypi
+# shellcheck disable=SC2086
+TWINE_USERNAME=__token__ TWINE_PASSWORD=${PYPI_TOKEN} twine upload \
+	${TWINE_OPTS} dist/*
