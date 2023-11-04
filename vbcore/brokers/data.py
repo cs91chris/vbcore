8a137268b9b6c53971273f55448b1cc8cd176561
cs91chris <cs91chris@voidbrain.me>
2023-11-04 17:13:41 +0100

    fix default headers for brokers

--- vbcore/brokers/data.py
+++ vbcore/brokers/data.py
@@ -16,14 +16,22 @@ class BrokerOptions(BaseDTO):
 
 @dataclass(frozen=True, kw_only=True)
 class Header(BaseDTO):
-    correlation_id: str = field(default_factory=lambda: str(uuid.uuid4()))
-    timestamp: float = field(default_factory=lambda: datetime.now().timestamp())
+    @staticmethod
+    def default_correlation_id() -> str:
+        return str(uuid.uuid4())
+
+    @staticmethod
+    def current_timestamp() -> float:
+        return datetime.now().timestamp()
+
+    correlation_id: str = field(default_factory=default_correlation_id)
+    timestamp: float = field(default_factory=current_timestamp)
 
     @classmethod
     def from_dict(cls, *_, **kwargs) -> Self:
         return cls(
-            correlation_id=kwargs["correlation_id"],
-            timestamp=float(kwargs["timestamp"]),
+            correlation_id=kwargs.get("correlation_id") or cls.default_correlation_id(),
+            timestamp=float(kwargs.get("timestamp") or cls.current_timestamp()),
         )
 
     def to_dict(self, *_, **__) -> dict:
