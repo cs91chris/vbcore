bbc21c4c5df1ab84936a3a75240a3c35612dde4e
cs91chris <cs91chris@voidbrain.me>
2023-11-04 22:45:22 +0100

    fix requirements and lazy import of nats

--- vbcore/brokers/factory.py
+++ vbcore/brokers/factory.py
@@ -1,11 +1,17 @@
 from enum import Enum
 
+from vbcore.datastruct.lazy import LazyImporter
 from vbcore.factory import ItemEnumMeta, ItemEnumMixin, ItemFactory
 
 from .base import BrokerClientAdapter
 from .data import BrokerOptions
 from .dummy import DummyBrokerAdapter
-from .nats import NatsBrokerAdapter, NatsOptions
+
+NatsBrokerAdapter, NatsOptions = LazyImporter.import_many(
+    "vbcore.brokers.nats:NatsBrokerAdapter",
+    "vbcore.brokers.nats:NatsOptions",
+    message="you must install 'nats-py'",
+)
 
 
 class BrokerEnum(ItemEnumMixin[BrokerOptions], Enum, metaclass=ItemEnumMeta):
